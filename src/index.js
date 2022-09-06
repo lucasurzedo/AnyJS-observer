d494dd6a503df637767606071ca2cd9589b3d72b
lucasurzedo <lucasurzedo1@gmail.com>
2022-09-06 18:03:09 -0300

    refactor(express): update index setupApp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-const { setupApp } = require('./express-server');
-const log = require('./log');
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+require('dotenv/config');
+
+const app = express();
+const port = 4447;
+
+async function setupApp() {
+  app.use(express.json());
+
+  app.use('/api/anyJS/v1', routes);
+
+  mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to DB!'));
+
+  return app;
+};
 
 setupApp()
   .then((app) => {
-    const port = 4447;
-    app.listen({ port }, () => log.info(`app running on port ${port}`));
+    app.listen({ port }, () => console.log(`app running on port ${port}`));
     return app;
   })
   .catch((error) => {
-    log.error(error);
+    console.log(error);
   });
